521194bcd92398b084692652c60014f245a6916e
dexiou <imuser@qq.com>
2019-07-31 09:22:09 +0800

    Update read_example.py
    fix the variable name in the loop.

--- tests/examples/python/read_example.py
+++ tests/examples/python/read_example.py
@@ -70,14 +70,8 @@ if __name__ == '__main__':
     # Use fetchall to fetch data in a list
     data = c1.fetchall()
 
-    try:
-        c1.execute('select * from db.t')
-    except Exception as err:
-        conn.close()
-        raise(err)
-
     # Use iterator to go through the retreived data
-    for col in c1:
+    for col in data:
         print(col)
 
     conn.close()
